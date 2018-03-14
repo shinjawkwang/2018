40f92243dcf836fc4c16c3ffca3d5d9a7fe642a9
shinjawkwang <shinjawkwang@naver.com>
2018-03-14 14:09:45 +0900

    SoftwareExperiment-by eclipse Initiation

---
+++ 01_SoftwareExperiment1/Practice1/src/Converter.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Converter {
+	public String toBinary(int num) {
+		String binary;
+		
+		
+		return binary;
+	}
+	public String toOctal(int num) {
+		String octal;
+		
+		return octal;
+	}
+	public String toHexa(int num) {
+		String hexa;
+		
+		return hexa;
+	}
+	
+	public static void main(String argc[]) {
+		int num;
+		Scanner inptnum = new Scanner(System.in);
+		System.out.println("Input decimal number: ");
+		num = inptnum.nextInt();
+		System.out.print(num);
+	}
+	
+}

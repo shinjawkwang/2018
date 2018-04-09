521c2d31e277ad84030a65573ffb947c041da84a
shinjawkwang <shinjawkwang@naver.com>
2018-04-09 16:34:51 +0900

    SystemProgramming First Quiz path changed && First Assignment Initialize

---
+++ 02_SystemProgramming/Assignment1/sfp.c
@@ -0,0 +1,21 @@
+#include "sfp.h"
+
+sfp int2sfp(int input){
+	
+}
+
+int sfp2int(sfp input){
+}
+
+sfp float2sfp(float input){
+}
+
+float sfp2float(sfp input){
+}
+
+sfp sfp_add(sfp a, sfp b){
+}
+
+
+char* sfp2bits(sfp result){
+}
